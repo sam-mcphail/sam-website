b59bb0c0408cfd3fbf9e9783590a7c2afb9d9eae
Sam McPhail <sam.mcphail@hotmail.co.nz>
2024-04-05 23:53:00 +1300

    Adds cards for coding page

--- client/components/Coding.tsx
+++ client/components/Coding.tsx
@@ -9,22 +9,37 @@ export default function Coding() {
       <Header />
       <div className="coding-page">
         <div className="page-title">
-          <h1>This is the coding page!</h1>
+          <h1>Projects</h1>
         </div>
         <div className="projects">
-          <h2>Projects</h2>
-          <Link to="/football-team-builder">
-            <p>Football Team Builder</p>
-          </Link>
-          <Link to="/wordle">
-            <p>Wordle Clone</p>
-          </Link>
-          <Link to="/calculator">
-            <p>Calculator App</p>
-          </Link>
-          <Link to="/tic-tac-toe">
-            <p>Tic Tac Toe</p>
-          </Link>
+          <div className="project-card">
+            <h1>Football Team Builder</h1>
+            <p>Information about the project</p>
+            <Link to="/football-team-builder">
+              <button>Check it out</button>
+            </Link>
+          </div>
+          <div className="project-card">
+            <h1>Wordle Clone</h1>
+            <p>Information about the project</p>
+            <Link to="/wordle">
+              <button>Check it out</button>
+            </Link>
+          </div>
+          <div className="project-card">
+            <h1>Calculator App</h1>
+            <p>Information about the project</p>
+            <Link to="/calculator">
+              <button>Check it out</button>
+            </Link>
+          </div>
+          <div className="project-card">
+            <h1>Tic Tac Toe</h1>
+            <p>Information about the project</p>
+            <Link to="/tic-tac-toe">
+              <button>Check it out</button>
+            </Link>
+          </div>
         </div>
       </div>
       <Footer />
--- client/components/tic-tac-toe-app/TicTacToeGame.tsx
+++ client/components/tic-tac-toe-app/TicTacToeGame.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaSquare } from 'react-icons/fa'
 
 const defaultGameBoard = [
   ['', '', ''],
@@ -52,20 +53,74 @@ const winChecks = [
 
 export default function TicTacToeGame() {
   const [gameBoard, setGameBoard] = useState(defaultGameBoard)
+  const [playedGameBoards, setPlayedGameBoards] = useState(new Array(0))
+  const [gamePlayed, setGamePlayed] = useState(false)
   const [playerOneTurn, setPlayerOneTurn] = useState(true)
   const [playerOneWin, setPlayerOneWin] = useState(false)
   const [playerTwoWin, setPlayerTwoWin] = useState(false)
+  const [gameDrawn, setGameDrawn] = useState(false)
+  const [totalP1Wins, setTotalP1Wins] = useState(0)
+  const [totalP2Wins, setTotalP2Wins] = useState(0)
+  const [totalDraws, setTotalDraws] = useState(0)
+  const [nextGameVisible, setNextGameVisible] = useState(false)
 
   function handleSqaureClick(col, row) {
-    console.log('Sqaure clicked: ' + row + ', ' + col)
-    if (isEmpty(col, row)) {
-      fillSquare(col, row)
-      let newPlayerOneTurn = !playerOneTurn
-      setPlayerOneTurn(newPlayerOneTurn)
+    if (!playerOneWin && !playerTwoWin && boardPlayable()) {
+      console.log('Sqaure clicked: ' + row + ', ' + col)
+      if (isEmpty(col, row)) {
+        fillSquare(col, row)
+        let newPlayerOneTurn = !playerOneTurn
+        setPlayerOneTurn(newPlayerOneTurn)
+      }
+      const winner = checkWin()
+      if (winner == 1) {
+        setPlayerOneWin(true)
+        setTotalP1Wins(totalP1Wins + 1)
+        gameFinished()
+      }
+      if (winner == 2) {
+        setPlayerTwoWin(true)
+        setTotalP2Wins(totalP2Wins + 1)
+        gameFinished()
+      }
+      if (!boardPlayable() && !playerOneWin && !playerTwoWin) {
+        setGameDrawn(true)
+        setTotalDraws(totalDraws + 1)
+        gameFinished()
+      }
     }
-    const winner = checkWin()
-    if (winner == 1) setPlayerOneWin(true)
-    if (winner == 2) setPlayerTwoWin(true)
+  }
+
+  function gameFinished() {
+    setGamePlayed(true)
+    setNextGameVisible(true)
+    console.log('next game visible')
+  }
+
+  function handleNextGame() {
+    let newPlayedGameBoard = [...playedGameBoards]
+    newPlayedGameBoard.push([...gameBoard])
+    setPlayedGameBoards(newPlayedGameBoard)
+    setGameBoard([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ])
+    setPlayerOneTurn(true)
+    setPlayerOneWin(false)
+    setPlayerTwoWin(false)
+    setGameDrawn(false)
+    setNextGameVisible(false)
+  }
+
+  function boardPlayable() {
+    let playable = false
+    gameBoard.map((row) => {
+      row.map((square) => {
+        if (square == '') playable = true
+      })
+    })
+    return playable
   }
 
   function checkWin() {
@@ -116,28 +171,72 @@ export default function TicTacToeGame() {
           <h2>Player Two's Turn (O)</h2>
         )}
       </div>
-      <div className="ttt-board">
-        {gameBoard.map((row, i) => (
-          <div key={i} className="ttt-board-row">
-            {row.map((square, j) => (
-              <div
-                key={j}
-                className="ttt-board-square"
-                onClick={() => handleSqaureClick(j, i)}
-              >
-                <p>{square}</p>
-              </div>
-            ))}
-          </div>
-        ))}
+      <div className="ttt-board-and-table">
+        <div className="ttt-board">
+          {gameBoard.map((row, i) => (
+            <div key={i} className="ttt-board-row">
+              {row.map((square, j) => (
+                <div
+                  key={j}
+                  className="ttt-board-square"
+                  onClick={() => handleSqaureClick(j, i)}
+                >
+                  <h1>{square}</h1>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="ttt-table">
+          <table>
+            <tr>
+              <th className="ttt-table-element">Player 1</th>
+              <th className="ttt-table-element">Draw</th>
+              <th className="ttt-table-element">Player 2</th>
+            </tr>
+            <tr>
+              <td className="ttt-table-element">{totalP1Wins}</td>
+              <td className="ttt-table-element">{totalDraws}</td>
+              <td className="ttt-table-element">{totalP2Wins}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div className="ttt-next-game">
+        {nextGameVisible ? (
+          <button onClick={() => handleNextGame()}>Next game</button>
+        ) : null}
       </div>
       <div className="ttt-player-win">
         {playerOneWin ? (
           <h2>Player One Wins!</h2>
         ) : playerTwoWin ? (
           <h2>Player Two Wins!</h2>
+        ) : gameDrawn ? (
+          <h2>Game Drawn!</h2>
         ) : null}
       </div>
+
+      {gamePlayed ? (
+        <>
+          <h2>Previous Games</h2>
+          <div className="ttt-prev-boards">
+            {playedGameBoards.map((board) => (
+              <div className="ttt-prev-board">
+                {board.map((row) => (
+                  <div className="ttt-prev-board-row">
+                    {row.map((square) => (
+                      <div className="ttt-prev-board-square">
+                        <h3>{square}</h3>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      ) : null}
     </div>
   )
 }
